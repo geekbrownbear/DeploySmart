e6f3061dbcada9b1ce81ca841d8c686d0a3f8860
Mattias Magnusson <6743307+mattish91@users.noreply.github.com>
2026-05-15 01:04:11 +0200

    Update Nextcloud Talk installation script to use API
    Made this more sturdy.

--- deployment/scripts/apps/Nextcloud_Talk.ps1
+++ deployment/scripts/apps/Nextcloud_Talk.ps1
@@ -3,10 +3,14 @@
 $ProgressPreference = 'SilentlyContinue'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $tp = "C:\TempPath" 
-$Installer = "Nextcloud.Talk-windows-x64.msi"
 mkdir $tp -EA SilentlyContinue
-$Url = "https://github.com/nextcloud-releases/talk-desktop/releases/latest/download/Nextcloud.Talk-windows-x64.msi"
-Invoke-WebRequest -Uri $Url -OutFile $tp\$Installer
-Start-Process msiexec.exe -Wait -ArgumentList "/norestart /i $tp\$Installer /qn"
-Start-Sleep -Seconds 30
-del $tp\$Installer
+$owner = "nextcloud-releases"
+$repo = "talk-desktop"
+$latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/$owner/$repo/releases/latest"
+$asset = $latestRelease.assets | Where-Object { $_.name -like "Nextcloud.Talk-windows-x64.msi" } | Select-Object -First 1
+$assetUrl = $asset.browser_download_url
+$installerPath = "$tp\$($asset.name)"
+Invoke-WebRequest -Uri $assetUrl -OutFile $installerPath
+Start-Process msiexec.exe -Wait -ArgumentList "/norestart /i $installerPath /qn"
+Start-Sleep -Seconds 10
+del $installerPath -Force
